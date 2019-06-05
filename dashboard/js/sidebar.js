52d074e31e5f083f07fe64d6511b894c4239387a
lidewij <lidewij122@gmail.com>
2019-06-05 10:50:44 +0200

    added changes to sub-page

--- dashboard/js/sidebar.js
+++ dashboard/js/sidebar.js
@@ -1,5 +1,5 @@
-// const sidebarNavTop = document.querySelector('.sidebar-nav-top');
-//
-//
-//
-// console.log(sidebarNavTop);
+const sidebarNavTop = document.querySelector('.sidebar-nav-top');
+
+sidebarNavTop.classList.remove('flex-column');
+
+console.log(sidebarNavTop.classList);
