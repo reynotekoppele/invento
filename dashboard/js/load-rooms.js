d25ca0e67025f4a9f724efd15d3ee8d4a43e91f1
lidewij <lidewij122@gmail.com>
2019-06-13 10:55:46 +0200

    hover

--- dashboard/js/load-rooms.js
+++ dashboard/js/load-rooms.js
@@ -1,4 +1,6 @@
 //haal alle rooms op uit de database
+const user = JSON.parse(Cookies.get("user"));
+
 fetch("/api/result/room", {
   method: "GET",
   headers: {
@@ -8,7 +10,7 @@ fetch("/api/result/room", {
 }).then(data => {
   return data.json();
 }).then(rooms => {
-
+  rooms = rooms.filter(room => room.user_id === user.id);
   const roomsWrapper = document.querySelector("#rooms"); //laad rooms wrapper element
 
   const row = document.createElement("div"); //maak een nieuwe row
@@ -16,10 +18,11 @@ fetch("/api/result/room", {
 
   //loop door iedere room
   rooms.forEach((room, index) => {
+
     const roomButton = document.createElement("div"); //maak room button
     roomButton.classList.add("knop", "col-md-3");
 
-     //vul de roombutton met de juiste waardes
+    //vul de roombutton met de juiste waardes
     roomButton.innerHTML = "<a class='btn-knop' href='sub-page.html'>" +
       "<img class='img-size-simul svg' src='" + room.icon + "' alt='hal'>" +
       "<h4 class='white-text'>" + room.name + "</h4>" +
