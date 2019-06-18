baf5f87221b6241e7e66f26e8ce91b25d41b74c8
Dennis <dennis@dhmediadesign.nl>
2019-06-18 09:43:00 +0200

    stats added to navigation

--- dashboard/js/sub-page.js
+++ dashboard/js/sub-page.js
@@ -2,3 +2,30 @@ const url = new URL(window.location.href);
 const room = url.searchParams.get("room");
 
 document.body.innerHTML = document.body.innerHTML.replace(/@ROOM@/g, room);
+
+function changeActiveDay() {
+  var element = document.getElementById("dag");
+  element.classList.add("active");
+  var element = document.getElementById("nacht");
+  element.classList.remove("active");
+  var element = document.getElementById("gezel");
+  element.classList.remove("active");
+};
+
+function changeActiveNight() {
+  var element = document.getElementById("nacht");
+  element.classList.add("active");
+  var element = document.getElementById("dag");
+  element.classList.remove("active");
+  var element = document.getElementById("gezel");
+  element.classList.remove("active");
+};
+
+function changeActiveGezel() {
+  var element = document.getElementById("gezel");
+  element.classList.add("active");
+  var element = document.getElementById("nacht");
+  element.classList.remove("active");
+  var element = document.getElementById("dag");
+  element.classList.remove("active");
+};
